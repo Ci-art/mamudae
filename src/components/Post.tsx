0fe7918833406aa313ef3fc33b72bd496f65f1ae
Pdom <ahb0327@naver.com>
2020-12-23 05:34:50 +0900

    Revert "Update Post.tsx"
    This reverts commit 0a6e2a20644ba710e9af39a39f6f8991a2d047c1.

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import { PostData } from '../store/postStore';
-import { Card, CardBody, CardHeader, CardText } from 'reactstrap';
 
 export interface PostProps {
   data: PostData;
 }
 
 export const Post: React.FC<PostProps> = (props) => {
-  return (
-    <Card>
-      <CardHeader tag="h5">{props.data.title}</CardHeader>
-      <CardBody>
-        <CardText>{props.data.content}</CardText>
-      </CardBody>
-    </Card>
-  );
+  return <li>{JSON.stringify(props.data)}</li>;
 };
