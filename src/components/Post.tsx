0a6e2a20644ba710e9af39a39f6f8991a2d047c1
Pdom <ahb0327@naver.com>
2020-12-23 04:58:26 +0900

    Update Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { PostData } from '../store/postStore';
+import { Card, CardBody, CardHeader, CardText } from 'reactstrap';
 
 export interface PostProps {
   data: PostData;
 }
 
 export const Post: React.FC<PostProps> = (props) => {
-  return <li>{JSON.stringify(props.data)}</li>;
+  return (
+    <Card>
+      <CardHeader tag="h5">{props.data.title}</CardHeader>
+      <CardBody>
+        <CardText>{props.data.content}</CardText>
+      </CardBody>
+    </Card>
+  );
 };
