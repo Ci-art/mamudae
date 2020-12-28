4354a8511f03a1f002001c70cd4cfdf6ba3f4d52
Pdom <ahb0327@naver.com>
2020-12-29 03:11:35 +0900

    Add tailwindcss-filters

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,38 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'media',
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      primary: colors.indigo,
+      danger: colors.rose,
+    },
     container: {
       center: true,
+      padding: '1rem',
+    },
+    filter: {
+      none: 'none',
+      grayscale: 'grayscale(1)',
+      invert: 'invert(1)',
+      sepia: 'sepia(1)',
+    },
+    backdropFilter: {
+      none: 'none',
+      blur: 'blur(20px)',
     },
   },
   variants: {
+    filter: ['responsive'],
+    backdropFilter: ['responsive'],
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwindcss-filters')],
 };
