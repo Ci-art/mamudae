765374b3d611d457805d2d14aac94ad277dcec19
Pdom <ahb0327@naver.com>
2020-12-29 03:15:43 +0900

    Update Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -6,5 +6,12 @@ export interface PostProps {
 }
 
 export const Post: React.FC<PostProps> = (props) => {
-  return <li>{JSON.stringify(props.data)}</li>;
+  return (
+    <div className="p-4 bg-white bg-opacity-70 backdrop-blur border border-gray-200 rounded-2xl transition-shadow hover:shadow-md">
+      <h2 className="mb-2 font-bold text-black text-opacity-90 text-4xl">
+        {props.data.title}
+      </h2>
+      <p>{props.data.content}</p>
+    </div>
+  );
 };
