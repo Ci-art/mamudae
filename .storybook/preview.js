81c666f49c6ebd6d519dc16969ef69ccc67413f4
Pdom <ahb0327@naver.com>
2020-12-31 23:15:57 +0900

    Add storybook-react-router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
+import { addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
 import 'tailwindcss/tailwind.css';
+
+addDecorator(StoryRouter());
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
