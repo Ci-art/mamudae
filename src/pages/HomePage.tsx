efea52062d59c05a25ed9a3db8154b40d9e856c9
Pdom <ahb0327@naver.com>
2021-01-04 02:28:11 +0900

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Header } from '../containers/Header';
 import { PostList } from '../containers/PostList';
 
 export const HomePage: React.FC = () => {
   return (
     <>
+      <Helmet>
+        <title>메무대</title>
+      </Helmet>
       <Header />
       <div className="container my-4">
         <PostList />
