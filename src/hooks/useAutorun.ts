654b117969ee5472d8bfc0237c558fbbb0a45881
Pdom <ahb0327@naver.com>
2021-02-12 12:29:33 +0900

    Create useAutorun.ts

---
+++ src/hooks/useAutorun.ts
@@ -0,0 +1,11 @@
+import { autorun, IAutorunOptions, IReactionPublic } from 'mobx';
+import { useEffect } from 'react';
+
+export const useAutorun = (
+  view: (r: IReactionPublic) => any,
+  opts?: IAutorunOptions | undefined
+) => {
+  useEffect(() => {
+    autorun(view, opts);
+  }, []);
+};
