e10f4e7f43aec3ee2059e22aba6ff8c3042b59b3
Pdom <ahb0327@naver.com>
2021-02-12 12:32:17 +0900

    Update Admin.tsx

--- src/containers/Admin.tsx
+++ src/containers/Admin.tsx
@@ -1,21 +1,46 @@
-import { autorun } from 'mobx';
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import rootStore from '../store';
 import { useAxios } from './../hooks/useAxios';
+import { UserResult } from '../../functions/src/types';
+import { useAutorun } from '../hooks/useAutorun';
+import { Card } from '../components/Card';
+import { Button } from '../components/Button';
 
 export const Admin: React.VFC = () => {
   const { authStore } = rootStore;
   const axiosInstance = useAxios();
+  const [data, setData] = useState<UserResult[]>();
 
-  useEffect(() =>
-    autorun(() => {
-      if (authStore.isLogin) {
-        axiosInstance.get('users').then((value) => {
-          console.log(value.data);
-        });
-      }
-    })
-  );
+  const getUsers = () => {
+    axiosInstance.get<{ users?: UserResult[] }>('users').then((value) => {
+      setData(value.data.users);
+    });
+  };
 
-  return <div></div>;
+  useAutorun(() => {
+    if (authStore.isLogin) {
+      getUsers();
+    } else {
+      setData(undefined);
+    }
+  });
+
+  if (data) {
+    return (
+      <>
+        <Button title="새로고침" onClick={getUsers}></Button>
+        {data.map((value) => {
+          return (
+            <Card key={value.uid}>
+              <p>이메일: {value.email}</p>
+              <p>역할: {value.roles}</p>
+              <Button title="역할 변경"></Button>
+            </Card>
+          );
+        })}
+      </>
+    );
+  } else {
+    return <>로딩 중</>;
+  }
 };
