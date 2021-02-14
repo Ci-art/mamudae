091192f1061a4be6b3db03a257473567d58eb1c5
Pdom <ahb0327@naver.com>
2021-02-14 23:21:09 +0900

    Update Admin.tsx

--- src/containers/Admin.tsx
+++ src/containers/Admin.tsx
@@ -1,46 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import rootStore from '../store';
-import { useAxios } from './../hooks/useAxios';
 import { UserResult } from '../../functions/src/types';
 import { useAutorun } from '../hooks/useAutorun';
 import { Card } from '../components/Card';
 import { Button } from '../components/Button';
+import { useFunctions } from './../hooks/useFunctions';
 
 export const Admin: React.VFC = () => {
   const { authStore } = rootStore;
-  const axiosInstance = useAxios();
-  const [data, setData] = useState<UserResult[]>();
-
-  const getUsers = () => {
-    axiosInstance.get<{ users?: UserResult[] }>('users').then((value) => {
-      setData(value.data.users);
-    });
-  };
+  const [{ data, loading, error }, refetch] = useFunctions<{
+    users: UserResult[];
+  }>('users');
 
   useAutorun(() => {
-    if (authStore.isLogin) {
-      getUsers();
-    } else {
-      setData(undefined);
+    if (!authStore.isLoading) {
+      refetch();
     }
   });
 
-  if (data) {
-    return (
-      <>
-        <Button title="새로고침" onClick={getUsers}></Button>
-        {data.map((value) => {
-          return (
-            <Card key={value.uid}>
-              <p>이메일: {value.email}</p>
-              <p>역할: {value.roles}</p>
-              <Button title="역할 변경"></Button>
-            </Card>
-          );
-        })}
-      </>
-    );
-  } else {
-    return <>로딩 중</>;
-  }
+  return (
+    <>
+      {loading ? <p>로딩 중</p> : <></>}
+      <Button title="새로고침" onClick={() => refetch()}></Button>
+      {data?.users.map((value) => {
+        return (
+          <Card key={value.uid}>
+            <p>이메일: {value.email}</p>
+            <p>역할: {value.roles}</p>
+            <Button title="역할 변경"></Button>
+          </Card>
+        );
+      })}
+    </>
+  );
 };
