8a3e4613a930b5e044464ba5b2d9fccca923b885
Pdom <ahb0327@naver.com>
2021-02-15 16:25:22 +0900

    Update Admin.tsx

--- src/containers/Admin.tsx
+++ src/containers/Admin.tsx
@@ -5,12 +5,16 @@ import { useAutorun } from '../hooks/useAutorun';
 import { Card } from '../components/Card';
 import { Button } from '../components/Button';
 import { useFunctions } from './../hooks/useFunctions';
+import { UserRequest } from './../../functions/src/types';
 
 export const Admin: React.VFC = () => {
   const { authStore } = rootStore;
   const [{ data, loading, error }, refetch] = useFunctions<{
     users: UserResult[];
   }>('users');
+  const [, update] = useFunctions<{
+    user: UserRequest;
+  }>({ method: 'PATCH' }, { manual: true });
 
   useAutorun(() => {
     if (!authStore.isLoading) {
@@ -19,18 +23,30 @@ export const Admin: React.VFC = () => {
   });
 
   return (
-    <>
-      {loading ? <p>로딩 중</p> : <></>}
+    <div className="container">
+      {loading || !data?.users ? (
+        <p className="fixed m-auto">로딩 중</p>
+      ) : (
+        <></>
+      )}
       <Button title="새로고침" onClick={() => refetch()}></Button>
       {data?.users.map((value) => {
         return (
           <Card key={value.uid}>
             <p>이메일: {value.email}</p>
-            <p>역할: {value.roles}</p>
-            <Button title="역할 변경"></Button>
+            <p>역할: {JSON.stringify(value.roles)}</p>
+            <Button
+              title="역할 변경"
+              onClick={() =>
+                update({
+                  url: `users/${value.uid}`,
+                  data: { roles: { admin: true } },
+                })
+              }
+            ></Button>
           </Card>
         );
       })}
-    </>
+    </div>
   );
 };
